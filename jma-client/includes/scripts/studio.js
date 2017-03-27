ceb4564ed3f39b7a7d315efa1356c27ee760175b
Oran <Oran.moshe@gmail.com>
2017-03-28 01:36:17 +0300

    Studio - Scrollers design , layout components - events handlers on scrolling

--- jma-client/includes/scripts/studio.js
+++ jma-client/includes/scripts/studio.js
@@ -4,6 +4,19 @@ var unit_width = 10;
 var z_index = 3;
 var samples = new Array();
 var p = new player();
+var data = null;
+
+function getData(){
+	var tmp = null;
+	$.ajax({
+		dataType: "json",
+		url: "includes/data/data.json",
+		success:  function(result){
+			console.log('success load slogans.json');
+			drawGrid(unit_width, result);
+		}
+	});	
+}
 
 function zoomIn(){
 	if(unit_width>20)
@@ -18,7 +31,7 @@ function zoomOut(){
 	zoom();
 }
 
-function DrawGrid(unit_width){
+function drawGrid(unit_width,data){
 		var cells = Math.round(max_time/time_units);				
 		// intialize elements
 		$('#channels_list').html('');
@@ -29,22 +42,33 @@ function DrawGrid(unit_width){
 				$('.channels_grid_title').append('<div class="time-box">'+(i*time_units+time_units)+'</div><div class="time-box-border"></div>');
 			}
 		// generate grid rows
-		for (var i = 0; i < samples.length; i++) {
-			drawChannel(i,cells);
+		for (var i = 0; i < data.project.tracks[0].track.channels.length; i++) {
+			drawChannel(i,cells,data);
 		}	
 		// draw cursorLine
 		zoom();	
 	}		
 
-function drawChannel(i,cells){
+function drawChannel(i,cells,data){
+	var samples = data.project.tracks[0].track.channels[i].channel.samples;
+	var channel = data.project.tracks[0].track.channels[i].channel;
 	// create grid row
 	var row = $('<div><article class="channel_list_row" ></article><article class="channel_grid_row" ondrop="drop(event)" ondragover="allowDrop(event)" id="channel_grid_row_'+i+'"></article></article></div>');
+	// create and append, grid cells to grid row
+	var svg = '<?xml version="1.0" ?><svg height="20px" class="channel_play" version="1.1" viewBox="0 0 20 20" width="20px" xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="none" fill-rule="evenodd" id="Page-1" stroke="none" stroke-width="1"><g  id="Icons-AV" transform="translate(-126.000000, -85.000000)"><g id="play-circle-fill" transform="translate(126.000000, 85.000000)"><path d="M10,0 C4.5,0 0,4.5 0,10 C0,15.5 4.5,20 10,20 C15.5,20 20,15.5 20,10 C20,4.5 15.5,0 10,0 L10,0 Z M8,14.5 L8,5.5 L14,10 L8,14.5 L8,14.5 Z" id="Shape"/></g></g></g></svg>';
+	var list_item = $('<article class="channel_list_row_info">'+svg+' <div class="channel_name">'+ channel.name +'<div><div class="channel_details">'+channel.userId+' - '+ channel.instrument +'<div></article><article class="channel_list_row_btns"><ul><li></li><li class="btn_eye"></li><li class="btn_mic"></li></article>');
+	$(row).find('.channel_list_row').append(list_item);
 	// create and append, grid cells to grid row
 	for (var j = 0; j < cells; j++) {
 		$(row).find('.channel_grid_row').append('<div class="time-box"></div><div class="time-box-border"></div>');
 	}
 	// create and append, grid sample placeholder to grid row
-	$(row).find('.channel_grid_row').append('<article class="sample_placeholder" id="sample_placeholder_'+ i +'"><article class="sample" id="sample'+ i +'" ondragstart="drag(event)" draggable="true" data-index="'+i+'" data-time="'+samples[i].time+'" data-start="'+samples[i].start+'"></article>');
+	$(row).find('.channel_grid_row').append('<article class="sample_placeholder" id="sample_placeholder_'+ i +'"></article>');
+	// create and append samples on placeolder
+	for (var j = 0; j < samples.length; j++) {
+		$(row).find('.channel_grid_row .sample_placeholder').append('<article class="sample" id="'+samples[j].sample.sampleId+'" ondragstart="drag(event)" draggable="true" data-index="channel'+j+'sample'+i+'"></article>');
+		p.addSample(new Sample(samples[j].sample.sampleId,samples[j].sample.file.path,samples[j].sample.duration,samples[j].sample.start,1,channel.channelId));
+	}	
 	// append grid row to channel list
 	$('#channels_list').append(row);
 }	
@@ -64,8 +88,8 @@ function random(max){
 	return Math.floor((Math.random() * max) + 1);
 }
 
-function player(){
-
+function player(data){
+	player.samples = new Array();
 	player.timeouts = new Array();
 	player.intervals = new Array();
 	player.playTime = 0;
@@ -76,8 +100,19 @@ function player(){
 		$('#time').text(playTime);
 		$('#cursorLine').css({'left':secondsToOffset(playTime)});
 	}
-	this.play = function(playTime){
-			//console.log(samples);
+	
+	this.addSample = function(sample){
+		player.samples.push(sample);
+	}
+	this.getSample = function(id){
+		for (var i = player.samples.length - 1; i >= 0; i--) {
+			if(player.samples[i].id == id){
+				return player.samples[i];
+			}
+		}
+	};
+	this.playChannel = function(playTime,channel){
+	//console.log(samples);
 			if(player.isPlaying)
 				return;
 			player.isPlaying = true;
@@ -87,16 +122,17 @@ function player(){
 			},10);
 			player.intervals.push(st1);
 			// music
-			$(samples).each(function(index){
-				var el = $(this);
-				var timeToStartPlaying = parseFloat($(el)[0].start_)-parseFloat(player.playTime)/100;
+			console.log('samples');
+			$(player.samples).each(function(index){
+				var sample = player.samples[index];
+				console.log(sample);
+				var timeToStartPlaying = parseFloat(player.samples[index].start_)-parseFloat(player.playTime)/100;
 				var startAt = 0;
 				var remainToStart = 0;
 				// start at
-
 				if(timeToStartPlaying<0){
 					//console.log('remain to start:'+ index +':'+ timeToStartPlaying);
-					if(Math.abs(timeToStartPlaying)>$(el)[0].time_)
+					if(Math.abs(timeToStartPlaying)>player.samples[index].time_)
 						return;
 					remainToStart = 0;
 				}else{
@@ -113,8 +149,58 @@ function player(){
 				}
 				console.log(index + ': remainToStart:' + remainToStart);
 				var st = setTimeout(function(){
-					$(el)[0].audi.currentTime = startAt;
-					$(el)[0].audi.play();
+					console.log('>>');
+					console.log(sample.aud);
+					sample.aud.currentTime = startAt;
+					sample.aud.play();
+				},remainToStart*1000);	
+				player.timeouts.push(st);
+			});
+			//cursor					
+	    	$('#cursorLine').animate({'left':unit_width*max_time},max_time*1000, 'linear');
+	}
+	this.play = function(playTime){
+			//console.log(samples);
+			if(player.isPlaying)
+				return;
+			player.isPlaying = true;
+			var st1 =setInterval(function(){						
+				$('#time').text(++player.playTime/100);
+				//console.log(player.playTime/100);
+			},10);
+			player.intervals.push(st1);
+			// music
+			console.log('samples');
+			$(player.samples).each(function(index){
+				var sample = player.samples[index];
+				console.log(sample);
+				var timeToStartPlaying = parseFloat(player.samples[index].start_)-parseFloat(player.playTime)/100;
+				var startAt = 0;
+				var remainToStart = 0;
+				// start at
+				if(timeToStartPlaying<0){
+					//console.log('remain to start:'+ index +':'+ timeToStartPlaying);
+					if(Math.abs(timeToStartPlaying)>player.samples[index].time_)
+						return;
+					remainToStart = 0;
+				}else{
+					//console.log('remain to start:'+ timeToStartPlaying);
+					remainToStart = timeToStartPlaying;							
+				}
+			
+				if(timeToStartPlaying<0 ){
+					//console.log('start at:'+ timeToStartPlaying);
+					startAt = Math.abs(timeToStartPlaying);	
+				}else{
+					startAt = 0;
+					//console.log('start at:'+ 0);
+				}
+				console.log(index + ': remainToStart:' + remainToStart);
+				var st = setTimeout(function(){
+					console.log('>>');
+					console.log(sample.aud);
+					sample.aud.currentTime = startAt;
+					sample.aud.play();
 				},remainToStart*1000);	
 				player.timeouts.push(st);
 			});
@@ -125,9 +211,8 @@ function player(){
 		console.log('pause');
 		player.isPlaying = false;
 		// music
-			$(samples).each(function(){
-				var el = $(this);
-				$(el)[0].audi.pause();
+			$(player.samples).each(function(index){
+				player.samples[index].aud.pause();
 			});		
 			reset();		
 		// cursor
@@ -137,11 +222,9 @@ function player(){
 		console.log('stop');
 		player.isPlaying = false;
 		// music
-			$(samples).each(function(){
-				var el = $(this);
-				console.log($(el)[0]);
-				$(el)[0].audi.pause();
-				$(el)[0].audi.currentTime = 0;
+			$(player.samples).each(function(index){
+				player.samples[index].aud.pause();
+				player.samples[index].aud.currentTime = 0;
 			});		
 			$('#time').text(player.playTime=0);	
 			reset();	
@@ -172,7 +255,8 @@ var Channel = class Channel {
 }
 
 var Sample = class Sample {
-  constructor(file, time, start,volume,username,userId,type) {
+  constructor(id,file, time, start,volume,username,userId,type,channel) {
+    this.id = id;
     this.file = file;
     this.time = time;
     this.start = start;
@@ -181,8 +265,10 @@ var Sample = class Sample {
     this.aud.volume = volume;
     this.username = username;
     this.userId = userId;
-    this.type = type;		    
+    this.type = type;	
+    this.channel = channel;	    
   }
+
    get audi(){
    	return this.aud;
    }
@@ -220,16 +306,18 @@ function zoom(){
 	else
 		$('#cursorLine').css('left',secondsToOffset(player.playTime/100));
 	$('#cursorLine').css('height',$('.channel_grid_row').length*(unit_width*10+1)+24);
-
-	console.log(secondsToOffset(player.playTime/100));
-
 }
+
 function refreshSample(element){
-	var width = $(element).attr("data-time")*unit_width+ ($(element).attr("data-time")/time_units-2);
+	var sample = p.getSample(element.id);
+	console.log("<><><><>");
+	console.log(element.id);
+	var width = sample.time*unit_width+ (sample.time/time_units-2);
 	$(element).css('width',width);
-	$(element).css('left',secondsToOffset($(element).attr("data-start")));
-	//createSoundSpectrum(element,width,100);
+	$(element).css('left',secondsToOffset(sample.start));
+	createSoundSpectrum(element,width,100);
 }
+
 function createSoundSpectrum(sample,width,height){
 	if($(sample).find('canvas').length==0){
 		console.log('new can');
@@ -242,7 +330,7 @@ function createSoundSpectrum(sample,width,height){
 		};
 		drawSoundFile(function(data){
 			console.log(data);
-		},context,newCanvas,height,width,samples[$(sample).attr('data-index')].aud.src);
+		},context,newCanvas,height,width,p.getSample(sample.id).aud.src);
 		$(sample).append(newCanvas);
 	}
 }
@@ -251,8 +339,13 @@ function refreshSamples(){
 		refreshSample(this);
 	});	
 }
-function updateSampleStart(i,start){
-	samples[i].start_ = start;
+function updateSampleStart(id,start){
+	for (var i = player.samples.length - 1; i >= 0; i--) {
+		console.log(id);	
+		if(player.samples[i].id == id){
+			player.samples[i].start = start;
+		}
+	}
 }
 function offsetToSeconds(offset){
 	var borders = Math.floor(offset/((time_units*unit_width)));
@@ -291,6 +384,7 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     var dragged = document.getElementById(data);		   
     var target = ev.target;
+    console.log(dragged.id);
     var new_offset = $('main').scrollLeft()+ ev.pageX - mouse_offset - 160;
 	if(new_offset<0)
 		new_offset = 0;
@@ -313,7 +407,7 @@ function drop(ev) {
     var borders = Math.floor(new_offset/((time_units*unit_width)));
     var all = ((new_offset-borders)/(time_units*unit_width))*time_units;
     $('#'+data).attr('data-start', all);
-    updateSampleStart($('#'+data).attr('data-index'),$('#'+data).attr('data-start'));
+    updateSampleStart(data,all);
 	if(player.isPlaying){
 		p.pause();
 		p.play();
@@ -324,14 +418,23 @@ function drop(ev) {
 var mouse_offset;
 $(document).on('mousedown', function(e){
     mouse_offset = e.pageX - $(e.target).offset().left;
+
+    if($(e.target).hasClass('sample')){
+    	$(e.target).css('z-index', z_index++);
+        $('#channels_title').css('z-index', z_index++);
+        $('.channel_list_buttons').css('z-index', z_index++);
+    }
+    if($(e.target).parent().hasClass('sample')){
+    	$(e.target).parent().css('z-index', z_index++);
+        $('#channels_title').css('z-index', z_index++);
+        $('.channel_list_buttons').css('z-index', z_index++);
+    }
 });
 $(document).on('mouseup', function(e){
 
 });
 $(document).bind('click',function(el){	
-	if($(el.target).hasClass('sample'))
-    	$(el.target).css('z-index', z_index++);
-        $('#channels_title').css('z-index', z_index++);
+	
 });
 var shifted = false;
 $(document).on('keyup keydown', function(e){shifted = e.shiftKey} );
@@ -340,9 +443,7 @@ $(document).on('keyup keydown', function(e){shifted = e.shiftKey} );
 
 
 $(document).ready(function(){
-	initialize();
 	getData();
-	DrawGrid(unit_width);
 	$("#play").click(function(){
 		p.play();
 	});
@@ -352,7 +453,9 @@ $(document).ready(function(){
 	$("#stop").click(function(){
 		p.stop();
 	});
-	$(".channel_grid_row").on('click',function(e){
+	$("#channels_list").on('click',function(e){
+		if(offsetToSeconds($('main').scrollLeft()+ e.pageX  < 160))
+			return;
 		if(player.isPlaying){
 			p.pause();
 			p.setTime(offsetToSeconds($('main').scrollLeft()+ e.pageX  - 160));
@@ -361,9 +464,13 @@ $(document).ready(function(){
 		else
 			p.setTime(offsetToSeconds($('main').scrollLeft()+ e.pageX  - 160));				
 	});
+
 	$('main').scroll(function(){
 	    $('#channels_title').css({
 	        'top': $(this).scrollTop() 
+	    });
+	    $('.channel_list_row , .channel_list_buttons').css({
+	        'left': $(this).scrollLeft() 
 	    });
 	});
 
