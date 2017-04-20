ba73864a7dc75178487cabf7c29a6a36ba6761ed
maoz26 <maoz.tamir1@gmail.com>
2017-04-20 16:51:27 +0300

    plan schema

--- server/routes/plan.js
+++ server/routes/plan.js
@@ -4,9 +4,76 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
 var Plan   = require('../model/planSchema');  // get our mongoose model
 
-/* GET project. */
-router.get('/', ensureLoggedIn, function(req, res, next) {
-   res.send('hello world');
+
+/* get specific request */
+router.get('/plan:_id', function(req, res) {
+    res.redirect('http://localhost/jam/plan'); // for now will just redirect back to plan page
 });
+
+
+/* create new request function */
+router.post('/plan', function(req, res) {
+    // get params
+    var planId = req.body.planId;
+    var name = req.body.name;
+    var description = req.body.description;
+    // request structure
+    var newPlan = new Plan({
+        planId : '',
+        name: '',
+        description: ''
+    });
+    //set structure
+    newPlan.planId = planId;
+    newPlan.name = name;
+    newPlan.description = description;
+    //console.log(newPlan);
+    // save to database
+    newPlan.save(function(err) {
+        if (err) throw err;
+        console.log('Plan was added successfully');
+        res.json({ success: true });
+        //res.redirect('http://localhost/jam/plan');
+    });
+});
+
+
+/* delete request */
+router.post('/deletePlan', function(req, res) {
+    // get params
+    var planId = req.body.planId;
+    Plan.findOne({
+        plan: req.body.planId
+    }, function(err, plan) {
+        if (err) throw err;
+        if (!plan) {
+            res.json({ success: false, message: 'plan not found.'});
+        }
+        if(plan.planId === planId){
+            plan.remove(function(err) {
+                if (err) throw err;
+                console.log('User was removed successfully');
+                res.json({ success: true });
+                //res.redirect('http://localhost/jam/request');
+            });
+        }else{
+            res.json({ success: false, message: 'Can not delete request, call 911.'});
+        }
+    });
+});
+
+
+/* update request */
+router.put('/updatePlan:_id', function(req, res) {
+    Plan.findOne(req.params.planId, function(err,plan){
+        if (err) res.send(err);
+        plan.storage_capacity += req.body.storage_capacity;
+        plan.save(function(err){
+            if (err) res.send(err);
+            res.json({ message: "update plan"});
+        });
+    });
+});
+
 
 module.exports = router;
--- server/routes/request.js
+++ server/routes/request.js
@@ -4,76 +4,13 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
 var Request   = require('../model/requestSchema');  // get our mongoose model
 
-
-/*  */
-router.get('/request', function(req, res) {
-    res.redirect('http://localhost/jam/request'); // for now will just redirect back to request page
+/* GET project. */
+router.get('/', ensureLoggedIn, function(req, res, next) {
+    res.send('hello world');
 });
 
-
-/* create new request function */
-router.post('/request', function(req, res) {
-    // get params
-    var reqPro = req.body.projectid;
-    var reqUser = req.body.userid;
-    var reqStat = req.body.status;
-    // request structure
-    var tempReq = new Request({
-        projectId : '',
-        userId: '',
-        status: ''
-    });
-    //set structure
-    tempReq.projectId = reqPro;
-    tempReq.userId = reqUser;
-    tempReq.status = reqStat;
-    //console.log(tempUser);
-    // save to database
-    tempReq.save(function(err) {
-        if (err) throw err;
-        console.log('Request was added successfully');
-        res.json({ success: true });
-        //res.redirect('http://localhost/jam/request');
-    });
-});
-
-
-/* delete request */
-router.post('/deleteRequest', function(req, res) {
-    // get params
-    var reqId = req.body.requestId;
-    Request.findOne({
-        request: req.body._id
-    }, function(err, request) {
-        if (err) throw err;
-        if (!request) {
-            res.json({ success: false, message: 'Request not found.'});
-        }
-        if(request._id === reqId){
-            request.remove(function(err) {
-                if (err) throw err;
-                console.log('User was removed successfully');
-                res.json({ success: true });
-                //res.redirect('http://localhost/jam/request');
-            });
-        }else{
-            res.json({ success: false, message: 'Can not delete request, call 911.'});
-        }
-    });
-});
-
-
-/* update request */
-router.put('/deleteRequest:_id', function(req, res) {
-    Request.findOne(req.params._id, function(err,request){
-        if (err) res.send(err);
-        request.status = req.body.status;
-        request.save(function(err){
-           if (err) res.send(err);
-           res.json({ message: "update request"});
-        });
-    });
-});
-
+/*
+* deleted. will be implemented by mark
+* */
 
 module.exports = router;
