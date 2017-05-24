eaecd61a8a5cbf13ecac9eab53407812d55ae5af
markper <perzov@gmail.com>
2017-05-24 20:45:09 +0300

    Server - minor fix for request shchema & request route
    Server - minor fix for request shchema & request route

--- server/model/requestSchema.js
+++ server/model/requestSchema.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('Request', new Schema({
-    projectId: String,
-    userId: String,
+    requestId: String,
+    projectId:  { type: String, ref: 'Project' },
+    userId:  { type: String, ref: 'User' },
     status: String
 }));
