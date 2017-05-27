6dba4e648d7f53782486963ed2803a44bcf10be9
markper <perzov@gmail.com>
2017-05-27 16:55:58 +0300

    notification WS model & controler
    notification WS model  fix & controler implimentation + error handling.

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -5,39 +5,39 @@ var router = express.Router();
 var Notification = require('../model/notificationSchema');  // get our mongoose model
 var Project = require('../model/projectSchema');  // get our mongoose model
 var User = require('../model/userSchema');  // get our mongoose model
+var notificationCtrl = require('../controllers/notifications');
 
 /* POST - notification. */
 router.post('/', function(req, res, next) {
 	var notification = new Notification(req.body);
-	notification.save();
-	res.send(notification);
-});
-
-/* GET - notification. */
-router.get('/:notificationId', function(req, res, next) {
-	var notificationId = req.params.notificationId;
-	Notification
-	.findOne({_id:notificationId})
-	.populate({path:'factor',select: ['firstName','lastName']})
-	.exec(function (err, notification) {
-	  if (!err)
-		 res.send(notification);
-	  else
-	  	res.send('error');
+	notificationCtrl.newNotification(notification, function(data){
+	if(data instanceof Error)
+		res.status(500).send(data.message);
+	else
+		res.status(200).send(data);
 	});
 });
 
+/* GET - notification. */
+// done controllers
+router.get('/:notificationId', function(req, res, next) {
+	var notificationId = req.params.notificationId;
+  notificationCtrl.getNotification(notificationId, function(data){
+		if(data instanceof Error)
+			res.status(500).send(data.message);
+		else
+			res.status(200).send(data);
+	});
+});
+
+// done controllers
 router.get('/GetList/:userId', function(req, res, next) {
 	var userId = req.params.userId;
-	Notification
-	.findOne({"subscribes.user": userId})
-	.populate({path:'factor',select: ['firstName','lastName']})
-  .populate({path:'subscribes.user',select: ['firstName','lastName']})
-	.exec(function (err, notification) {
-	  if (!err)
-		 res.send(notification);
-	  else
-	  	res.send('error');
+	notificationCtrl.getNotificationByUser(userId, function(data){
+		if(data instanceof Error)
+			res.status(500).send(data.message);
+		else
+			res.status(200).send(data);
 	});
 });
 
@@ -46,15 +46,12 @@ router.put('/:notificationId/ReadByUser/:userId'/*,ensureLoggedIn*/, function(re
     var notificationId = req.params.notificationId;
     var userId = req.params.userId;
     req.body._id = userId;
-    Notification.update( {_id: notificationId, subscribes: { $elemMatch:{user: userId}}},
-    {$set: {"subscribes.$.read": true}}, function(err, notification){
-        if(err) {
-            return res.status(500).json({'error' : 'error in changing read flag'});
-        }
-        res.json(notification);
-
-    });
-
+		notificationCtrl.updateNotificationStatus(notificationId, userId, function(data) {
+			if(data instanceof Error)
+				res.status(500).send(data.message);
+			else
+				res.status(200).send(data);
+		});
 });
 
 
